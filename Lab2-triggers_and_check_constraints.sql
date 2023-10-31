7d1cf898c51d7ff063edb38d6d965070a4d8fbf7
Noela2 <88770312+Noela2@users.noreply.github.com>
2023-10-31 11:18:11 +0300

    products_undo table created
    This table will record the old values from the product table before any manipulation takes place.

--- Lab2-triggers_and_check_constraints.sql
+++ Lab2-triggers_and_check_constraints.sql
@@ -1,1 +1,19 @@
-
+-- Create a table that will record the old values of the product table before changes took place.
+-- date_change will record the time and date that the data manipulation took place.
+-- type_of _change records the type of data manipulation done
+CREATE TABLE `products_undo`
+(
+`date_changed` timestamp(2) NOT NULL DEFAULT CURRENT_TIMESTAMP(2),
+`productCode` varchar(15) NOT NULL,
+`productName` varchar(70) DEFAULT NULL,
+`productLine` varchar(50) DEFAULT NULL,
+`productScale` varchar(10) DEFAULT NULL,
+`productVendor` varchar(50) DEFAULT NULL,
+`productDescription` text DEFAULT NULL,
+`quantityInStock` smallint DEFAULT NULL,
+`buyPrice` decimal(10,2) NOT NULL,
+`MSRP` decimal(10,2) NOT NULL,
+`type_of_change` varchar(50) NOT NULL,
+PRIMARY KEY (`date_changed`),
+UNIQUE KEY `date_changed_UNIQUE` (`date_changed`)
+) ENGINE= InnoDB;
